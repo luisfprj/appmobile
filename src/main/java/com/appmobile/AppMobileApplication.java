b21e9eb2e2e606d5d75bd3713bd9489ce6185c72
luis.fprj <luis.fprj@gmail.com>
2018-08-13 20:13:43 -0300

    primeiro commit

---
+++ src/main/java/com/appmobile/AppMobileApplication.java
@@ -0,0 +1,12 @@
+package com.appmobile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppMobileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppMobileApplication.class, args);
+	}
+}
